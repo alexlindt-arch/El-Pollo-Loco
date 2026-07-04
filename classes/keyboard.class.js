f4f059722512948fe473737d51acc90905ad049f
alexlindt-arch <alex.lindt@t-online.de>
2026-07-04 10:56:41 +0200

    Add Keyboard class (input state)

---
+++ classes/keyboard.class.js
@@ -0,0 +1,11 @@
+/** Class representing the keyboard with its keys relevant to the game. */
+class Keyboard {
+    LEFT = false;
+    RIGHT = false;
+    UP = false;
+    DOWN = false;
+    SPACE = false;
+    D = false;
+
+    lastKeyPress = 0;
+}
